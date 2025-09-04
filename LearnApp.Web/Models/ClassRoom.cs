29de42a27e653dee2072d48e953d4bfdfafbd231
ET <ET@DESKTOP-9M4JH0K>
2025-09-04 17:42:56 +0330

    Add project files.

---
+++ LearnApp.Web/Models/ClassRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnApp.Web.Models
+{
+    public class ClassRoom
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ LearnApp.Web/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearnApp.Web.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+       
+        public int ClassRoomID { get; set; }
+
+        //[ForeignKey("ClassRoomId")]
+        public ClassRoom ClassRoom { get; set; }
+
+    }
+}
